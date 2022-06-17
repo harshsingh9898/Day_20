e6d9977ee5c507c65b5f0ccb461cf1070e4b8bd5
harshsingh9898 <73526355+harshsingh9898@users.noreply.github.com>
2022-06-18 04:29:21 +0530

    Add files via upload
    Analyse_Mood_expression

---
+++ MoodAnalyser.cs
@@ -0,0 +1,39 @@
+﻿using MoodAnalyser;
+
+namespace UnitTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [TestCategory("MOOD HAPPY")]
+        public void TestMethod1()
+        {
+            
+            string message = "I am HAPPY today";
+            
+            string expected = "happy";
+            
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer(message);
+           
+            string actual = moodAnalyzer.AnalyseMood();
+           
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        [TestCategory("MOOD SAD")]
+        public void TestMethod2()
+        {
+            
+            string message = "I am sad today";
+            
+            string expected = "sad";
+            
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer(message);
+            
+            string actual = moodAnalyzer.AnalyseMood();
+            
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
